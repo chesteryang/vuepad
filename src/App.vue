03552d42ca7fa67576ff192e1e1f5f62437d6ca8
Chester Yang <ChesterInShaw@gmail.com>
2019-07-06 10:15:11 -0600

    Add menu and a new page

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn flat href="/">Home</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn flat href="/autocomp">Auto Comp</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn flat href="/about">About</v-btn>
+      <v-spacer></v-spacer>
       <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -14,21 +20,16 @@
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld />
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  },
+  components: {},
   data() {
     return {
       //
